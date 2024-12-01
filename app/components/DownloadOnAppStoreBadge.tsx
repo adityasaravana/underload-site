6243079a92f104a9beaba28d7a984266d6d68517
Aditya Saravana <aditya.saravana@icloud.com>
2024-11-30 18:00:52 -0800

    Update DownloadOnAppStoreBadge.tsx

--- app/components/DownloadOnAppStoreBadge.tsx
+++ app/components/DownloadOnAppStoreBadge.tsx
@@ -3,21 +3,22 @@ import Link from "next/link";
 import { appStoreURL } from "../constants";
 
 export function DownloadOnAppStoreBadge() {
+    const size = 175;
     return (
         <div>
             <Link href={appStoreURL}>
-                <Image 
+                <Image
                     src="/images/appstorebadges/as-badge-black.svg"
-                alt="Download on the App Store"
-                width={150}
-                height={150}
+                    alt="Download on the App Store"
+                    width={size}
+                    height={size}
                     className="block dark:hidden"
                 />
-                <Image 
+                <Image
                     src="/images/appstorebadges/as-badge-white.svg"
-                alt="Download on the App Store"
-                width={150}
-                height={150}
+                    alt="Download on the App Store"
+                    width={size}
+                    height={size}
                     className="hidden dark:block"
                 />
             </Link>
