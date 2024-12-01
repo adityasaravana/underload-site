4f862519ed3eb865feb070f95d79cae0fd4a45a8
Aditya Saravana <aditya.saravana@icloud.com>
2024-11-30 17:57:32 -0800

    removed background from screenshot

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <div className="flex flex-col items-center justify-center min-h-screen text-center px-4">
         {/* <Image src="/images/logo-nobg.png" alt="Underload's Logo, a bold '1'" width={150} height={150} /> */}
         <h1 className="text-4xl md:text-7xl font-extrabold leading-tight mb-6 max-w-4xl text-underload pt-20">
-          The to-do list app that shows you one task at a time.
+          The to-do list app that shows you one task at a time, and <em>works with Apple Reminders</em>.
         </h1>
         <p className="text-xl md:text-2xl text-underload pt-10">
           Built for procrastinators, by procrastinators.
@@ -19,7 +19,7 @@ export default function Home() {
 
         <p className="text-underload pt-3">For iPhone, iPad, and Mac.</p>
 
-        <Image src="/images/underload-sc-light.jpeg" alt="A screenshot of the main view of Underload" width={500} height={500} />
+        <Image src="/images/underload-sc-light.png" alt="A screenshot of the main view of Underload" width={500} height={500} />
       </div>
     </main>
   );
